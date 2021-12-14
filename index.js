d35b876bff9f325079713bab68e7ee11f09bba2c
Jacob Gregor <67080552+JacobGregor@users.noreply.github.com>
2021-12-13 23:20:43 -0800

    refactored and added routes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const app = require('./lib/server');
 const { db } = require('./lib/model');
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3001;
 
 db.sync().then(() => {
-  app.start(PORT);
+	app.start(PORT);
 });
